7b51450e5f48a296c3ddacc22654d69461ec3ede
Lisa Herzberg <lisamarieherzberg@gmail.com>
2018-07-07 08:47:27 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// for chrome network debugging
+// GLOBAL.XMLHttpRequest = GLOBAL.originalXMLHttpRequest || GLOBAL.XMLHttpRequest;
+
+import { AppRegistry } from 'react-native';
+import App from './src/main';
+
+AppRegistry.registerComponent('typescriptRN', () => App);
